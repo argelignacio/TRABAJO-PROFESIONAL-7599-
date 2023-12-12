890969b0d46dac2461f0df9b9571aed20521e26a
lucasbilo <lbilo@fi.uba.ar>
2023-12-11 21:00:29 -0300

    Fix joiner

--- joiner.py
+++ joiner.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+import pandas as pd
 
 directorio_principal = os.getcwd()
 df_final = pd.DataFrame()
@@ -7,9 +8,9 @@ df_final = pd.DataFrame()
 
 for nombre_directorio in os.listdir(directorio_principal):
     ruta_directorio = os.path.join(directorio_principal, nombre_directorio)
-    if os.path.isdir(ruta_directorio) and nombre_directorio.startswith("METRICS"):
+    if os.path.isdir(ruta_directorio) and nombre_directorio.startswith("METRICAS"):
         for nombre_archivo in os.listdir(ruta_directorio):
-            if nombre_archivo.lower().startswith("metrics") and nombre_archivo.lower().endswith(".pkl"):
+            if nombre_archivo.lower().startswith("metrics") and nombre_archivo.lower().endswith(".bin"):
                 ruta_archivo = os.path.join(ruta_directorio, nombre_archivo)
                 with open(ruta_archivo, 'rb') as file:
                     datos = pickle.load(file)
