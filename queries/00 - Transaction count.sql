977d1b74e8aed0edc9bdd3c3075a5450310fb013
mvbattan <mvbattan@gmail.com>
2023-12-11 21:48:25 -0300

    Replacing images from old 00 notebook

--- queries/00 - Transaction count.sql
+++ queries/00 - Transaction count.sql
@@ -3,6 +3,9 @@ SELECT
   EXTRACT(MONTH FROM block_timestamp) as month,
   EXTRACT(DAY FROM block_timestamp) as day,
   SUM(IF(transaction_type <> 2, 1, 0)) as sum_legacy,
+  SUM(gas) as sum_gas,
+  SUM(gas_price / 1E18) as sum_gas_price,
+  SUM(value / 1E18) as sum_value,
   COUNT(*) as transaction_count
 FROM bigquery-public-data.goog_blockchain_ethereum_mainnet_us.transactions t
 WHERE block_timestamp >= '2020-01-01'
