eb9028e2a8f3d6664d364cbdd0f05d3803b89e8f
Nahuel Spiguelman <nspiguelman@fi.uba.ar>
2023-12-14 20:49:03 -0300

    refactor metric maker

--- metric_maker.py
+++ metric_maker.py
@@ -7,118 +7,126 @@ import pickle
 import os
 import matplotlib.pyplot as plt
 
-month = "07"
-year = "2023"
-
-for i in range(1, 32):
-    if i < 10:
-        nombre_archivo = f'{year}-{month}-0{i}.csv'
-    else:
-        nombre_archivo = f'{year}-{month}-{i}.csv'
-
-    nombre = os.path.splitext(nombre_archivo)[0]
-
-    ruta_completa = os.path.join(os.getcwd(), "METRICAS_"+nombre)
-    os.mkdir(ruta_completa)
-
-    edges = pd.read_csv(
-        nombre_archivo, usecols=["from_address", "to_address", "value", "nonce", "gas"]
-    )
-    G = nx.from_pandas_edgelist(
-        edges, source="from_address", target="to_address", create_using=nx.DiGraph()
-    )
-
-    metrics = {}
-
+def calculate_total_nodes(G, metrics):
     # Cantiadad de nodos totales
     total_nodes = G.number_of_nodes()
     metrics["total_nodes"] = total_nodes
+    return metrics, total_nodes
 
+def calculate_total_edges(G, metrics):
     # Cantidad de aristas totales
     total_edges = G.number_of_edges()
     metrics["total_edges"] = total_edges
+    return metrics, total_edges
 
-    # Agregaciones sobre los g- y g+
-    in_degree_values = [G.in_degree(n) for n in G.nodes]
-    out_degree_values = [G.out_degree(n) for n in G.nodes]
+def degree_values_plot(in_degree_values, out_degree_values, total_nodes, name):
     in_degree_values_plot = sorted(in_degree_values, reverse=True)
-    out_degree_values_plot = sorted(out_degree_values, reverse=True)
     in_degree_values_plot = np.array(in_degree_values_plot)
-    out_degree_values_plot = np.array(out_degree_values_plot)
     in_degree_values_plot = in_degree_values_plot / total_nodes
-    out_degree_values_plot = out_degree_values_plot / total_nodes
     in_degree_values_plot = np.cumsum(in_degree_values_plot)
+
+    out_degree_values_plot = sorted(out_degree_values, reverse=True)
+    out_degree_values_plot = np.array(out_degree_values_plot)
+    out_degree_values_plot = out_degree_values_plot / total_nodes
     out_degree_values_plot = np.cumsum(out_degree_values_plot)
     plt.plot(in_degree_values_plot, label="in-degree")
     plt.plot(out_degree_values_plot, label="out-degree")
     plt.legend()
     plt.title('Distribucion acumulativa de grados de entrada y salida')
-    plt.savefig(f'METRICAS_{nombre}/distribucion_acum.png')
+    plt.savefig(f'METRICAS_{name}/distribucion_acum.png')
 
-    metrics["mean_in"] = np.mean(in_degree_values)
-    metrics["max_in"] = np.max(in_degree_values)
-    metrics["min_in"] = np.min(in_degree_values)
+def metrics_aggregation(metrics, data, alias):
+    metrics[f"mean_{alias}"] = np.mean(data)
+    metrics[f"std_{alias}"] = np.std(data)
+    metrics[f"max_{alias}"] = np.max(data)
+    metrics[f"min_{alias}"] = np.min(data)
+    return metrics
 
-    metrics["mean_out"] = np.mean(out_degree_values)
-    metrics["max_out"] = np.max(out_degree_values)
-    metrics["min_out"] = np.min(out_degree_values)
-
-    out_degree_values = ""
-    in_degree_values = ""
-    gc.collect()
-
+def pagerank_metrics(G, metrics):
     # PageRank
     print("Calculando PageRank")
     pagerank = nx.pagerank(G)
     pagerank_for_metrics = list(pagerank.values())
-
-    metrics["mean_pagerank"] = np.mean(pagerank_for_metrics)
-    metrics["min_pagerank"] = np.min(pagerank_for_metrics)
-    metrics["max_pagerank"] = np.max(pagerank_for_metrics)
-
+    metrics = metrics_aggregation(metrics, pagerank_for_metrics, "pagerank")
     pagerank = ""
     pagerank_for_metrics = ""
     gc.collect()
+    return metrics
 
-    # HITS
+def hits_metrics(G, metrics):
     print("Calculando HITS")
     hits = nx.hits(G)
     hub_scores, authority_scores = hits
-
     hub_scores_list = [x for x in hub_scores.values()]
     authority_scores_list = [x for x in authority_scores.values()]
-
-    metrics["mean_hub_score"] = np.mean(hub_scores_list)
-    metrics["min_hub_score"] = np.min(hub_scores_list)
-    metrics["max_hub_score"] = np.max(hub_scores_list)
-
-    metrics["mean_authority_scores"] = np.mean(authority_scores_list)
-    metrics["min_authority_scores"] = np.min(authority_scores_list)
-    metrics["max_authority_scores"] = np.max(authority_scores_list)
-
+    metrics = metrics_aggregation(metrics, hub_scores_list, "hub_score")
+    metrics = metrics_aggregation(metrics, authority_scores_list, "authority_scores")
     hub_scores_list = ""
     authority_scores_list = ""
     hits = ""
     gc.collect()
+    return metrics
 
-    # Metricas sobre columnas
-    print("Calculando metricas sobre columnas")
+def column_metrics(metrics, data, column_name):
+    print(f"agregando columna {column_name}")
+    metrics[f"mean_{column_name}"] = data[column_name].mean()
+    metrics[f"std_{column_name}"] = data[column_name].std()
+    metrics[f"max_{column_name}"] = data[column_name].max()
+    metrics[f"min_{column_name}"] = data[column_name].min()
+    return metrics
 
-    edges["value"] = edges["value"].astype(float)
-    metrics["mean_value"] = edges["value"].mean()
-    metrics["max_value"] = edges["value"].max()
-    print("Calculando max_value")
-    metrics["mean_nonce"] = edges["nonce"].mean()
-    metrics["max_nonce"] = edges["nonce"].max()
-    metrics["min_nonce"] = edges["nonce"].min()
-    print("Calculando max_nonce")
-    metrics["mean_gas"] = edges["gas"].mean()
-    metrics["max_gas"] = edges["gas"].max()
-    metrics["min_gas"] = edges["gas"].min()
-    print("Calculando max_gas")
+def daily_metrics():
+    dates = [("05", "2023"), ("06", "2023"), ("07", "2023")]
+    for month, year in dates:
+        for i in range(1, 32):
+            if i < 10:
+                name_archivo = f'{year}-{month}-0{i}.csv'
+            else:
+                name_archivo = f'{year}-{month}-{i}.csv'
 
+            name = os.path.splitext(name_archivo)[0]
 
-    nombre = os.path.splitext(nombre_archivo)[0]
-    print(f"Diccionario escrito en METRICAS_{nombre}/metrics_{nombre}.bin")
-    with open(f"METRICAS_{nombre}/metrics_{nombre}.bin", 'wb') as file:
-        pickle.dump(metrics, file)
+            ruta_completa = os.path.join(os.getcwd(), "METRICAS_"+name)
+            try:
+                os.mkdir(ruta_completa)
+            except FileExistsError:
+                print("Reemplazando el archivo ya existente")
+            except Exception:
+                raise "Error no controlado"
+
+            edges = pd.read_csv(
+                name_archivo, usecols=["from_address", "to_address", "value", "nonce", "gas"]
+            )
+            G = nx.from_pandas_edgelist(
+                edges, source="from_address", target="to_address", create_using=nx.DiGraph()
+            )
+
+            metrics = {}
+
+            metrics, total_nodes = calculate_total_nodes(G, metrics)
+            metrics, _total_edges = calculate_total_edges(G, metrics)
+
+            # Agregaciones sobre los g- y g+
+            in_degree_values = [G.in_degree(n) for n in G.nodes]
+            out_degree_values = [G.out_degree(n) for n in G.nodes]
+            degree_values_plot(in_degree_values, out_degree_values, total_nodes, name)
+
+            metrics = metrics_aggregation(metrics, in_degree_values, "in")
+            metrics = metrics_aggregation(metrics, out_degree_values, "out")
+            out_degree_values = ""
+            in_degree_values = ""
+            gc.collect()
+
+            pagerank_metrics(G, metrics)
+            hits_metrics(G, metrics)
+            edges["value"] = edges["value"].astype(float)
+            metrics = column_metrics(metrics, edges, "value")
+            metrics = column_metrics(metrics, edges, "nonce")
+            metrics = column_metrics(metrics, edges, "gas")
+
+            name = os.path.splitext(name_archivo)[0]
+            print(f"Diccionario escrito en METRICAS_{name}/metrics_{name}.bin")
+            with open(f"METRICAS_{name}/metrics_{name}.bin", 'wb') as file:
+                pickle.dump(metrics, file)
+
+daily_metrics()
