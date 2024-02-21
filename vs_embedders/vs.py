94c349a35817896e855ce6bf7c366faf041438e6
lucasbilo <lbilo@fi.uba.ar>
2024-02-21 19:54:10 -0300

    merge logger

--- vs_embedders/vs.py
+++ vs_embedders/vs.py
@@ -17,26 +17,19 @@ df.to_csv("fake_graph.csv", index=False)
 embedding_matrix, ids = pipeline_v2(df)
 np.array(embedding_matrix[0]).dump("embedding_matrix.npy")
 pkl.dump(ids, open("ids.pkl", "wb"))
-logger.info("Embedding matrix shape: " + str(embedding_matrix[0].shape))
-logger.error("This is an error message")
-logger.warning("This is a warning message")
-logger.critical("This is a critical message")
-logger.debug("This is a debug message")
 
 # Now run node2vec on the graph
 G = nx.from_pandas_edgelist(df, 'from_address', 'to_address')
 node2vec = Node2Vec(G, dimensions=128, walk_length=16, num_walks=100, workers=2)
 model_2 = node2vec.fit(window=10, min_count=1, batch_words=4)
 np.array(model_2.wv.vectors).dump("node2vec_embedding_matrix.npy")
-logger.info("Node2Vec embedding matrix shape: " + str(model_2.wv.vectors.shape))
 
 # get the embeddings for the nodes
 embedding_matrix_1 = np.load("embedding_matrix.npy", allow_pickle=True)
 embedding_matrix_2 = np.load("node2vec_embedding_matrix.npy", allow_pickle=True)
 
-print("Embedding matrix 1: ", embedding_matrix_1.shape)
-print("Embedding matrix 2: ", embedding_matrix_2.shape)
-
+logger.info(f"Embedding matrix 1: {embedding_matrix_1}")
+logger.info(f"Embedding matrix 2: {embedding_matrix_2}")
 
 
 
