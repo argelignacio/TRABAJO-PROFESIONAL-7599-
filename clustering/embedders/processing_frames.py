ff51b6f7cb9d9a661f5e9911f45e219ffe8ba0fe
Nahuel <nspiguelman@fi.uba.ar>
2024-05-07 20:24:02 -0300

    fix general en main data real

--- clustering/embedders/processing_frames.py
+++ clustering/embedders/processing_frames.py
@@ -7,121 +7,70 @@ from clustering.embedders.all_v2.ModelV2 import ModelBuilder
 import os
 from datetime import datetime, timedelta
 
-def set_gpu(logger):
-    gpus = tf.config.list_physical_devices('GPU')
-    if gpus:
-        try:
-            tf.config.set_visible_devices(gpus[0], 'GPU')
-            logical_gpus = tf.config.list_logical_devices('GPU')
-            logger.debug(f"GPU set: {logical_gpus}")
-        except RuntimeError as e:
-            logger.error(e)
+class ProcessingFrames:
+    def __init__(self, df, logger) -> None:
+        self.logger = logger
+        self.df = df
+        ProcessingFrames.set_gpu(logger)
 
-def read_files(files):
-    return pd.concat((pd.read_csv(f) for f in files), ignore_index=True)
+    def set_gpu(logger):
+        gpus = tf.config.list_physical_devices('GPU')
+        if gpus:
+            try:
+                tf.config.set_visible_devices(gpus[0], 'GPU')
+                logical_gpus = tf.config.list_logical_devices('GPU')
+                logger.debug(f"GPU set: {logical_gpus}")
+            except RuntimeError as e:
+                logger.error(e)
 
-def clean_nodes(df):
-    df = df[~df.to_address.isna()]
-    df = df[~df.from_address.isna()]
-    df = df[df["from_address"] != df["to_address"]]
-    uniques = df['from_address'].append(df['to_address']).value_counts()
-    unique_values = uniques[uniques == 1]
-    filtered_df = df[~(df['from_address'].isin(unique_values.index) | df['to_address'].isin(unique_values.index))]
-    return filtered_df
-
-def create_ids(df, logger):
-    ids = {}
-    for i, id in enumerate(set(df['from_address']).union(set(df['to_address']))):
-        ids[id] = i
-    logger.debug("Ids created")
-    return ids
-
-def create_generator(df, ids, logger, config):
-    generator = GeneratorTriplet(df, ids, config, logger)
-    logger.debug("Generator created")
-    return generator
-
-def pipeline(files, logger, config):
-    df = read_files(files)
-    cleaned_df = clean_nodes(df)
-    addresses_ids = create_ids(cleaned_df, logger)
-    cleaned_df['value'] = cleaned_df['value'].astype(float)
-
-    model_v2_config = config["MODEL_V2"]
-    model = ModelBuilder(addresses_ids, EuclideanLoss, Adam, logger, model_v2_config)
-
-    generator = create_generator(cleaned_df, addresses_ids, logger, config)
-    embeddings = model.compile_model().fit(generator).get_embeddings()
-    return embeddings, addresses_ids
-
-def pipeline_v2(df, logger, config):
-    set_gpu(logger)
-    cleaned_df = clean_nodes(df)
-    addresses_ids = create_ids(cleaned_df, logger)
-
-    logger.debug("Creating model")
-    model_v2_config = config["MODEL_V2"]
-    model = ModelBuilder(addresses_ids, EuclideanLoss, Adam, logger, model_v2_config)
+    def build_from_files(files, logger):
+        df = pd.concat((pd.read_csv(f) for f in files), ignore_index=True)
+        df['value'] = df['value'].astype('float64') / 1e18
+        processing_frames = ProcessingFrames(df, logger)
+        return processing_frames
     
-    generator = create_generator(cleaned_df, addresses_ids, logger, config)
-    embeddings = model.compile_model().fit(generator).get_embeddings()
-    return embeddings, addresses_ids
+    def build_from_df(df, logger):
+        processing_frames = ProcessingFrames(df, logger)
+        return processing_frames
 
-dias_por_mes = {"June": 30, "July": 31}
+    def _clean_nodes(self):
+        df_tmp = self.df
+        df_tmp = df_tmp[~df_tmp.to_address.isna()]
+        df_tmp = df_tmp[~df_tmp.from_address.isna()]
+        df_tmp = df_tmp[df_tmp["from_address"] != df_tmp["to_address"]]
+        uniques = df_tmp['from_address']._append(df_tmp['to_address']).value_counts()
+        unique_values = uniques[uniques == 1]
+        return df_tmp[~(df_tmp['from_address'].isin(unique_values.index) | df_tmp['to_address'].isin(unique_values.index))]
 
-def obtener_archivos(ruta_directorio):
-    return [os.path.join(ruta_directorio, archivo) for archivo in os.listdir(ruta_directorio)]
+    def _create_ids(self, cleaned_df):
+        ids = {}
+        for i, id in enumerate(set(cleaned_df['from_address']).union(set(cleaned_df['to_address']))):
+            ids[id] = i
+        self.logger.debug("Ids created")
+        return ids
 
-def obtener_fechas_ventana(fecha_inicial, ventana):
-    fechas = []
-    for i in range(0, dias_por_mes[fecha_inicial.strftime("%B")], ventana):
-        ventana_inicio = fecha_inicial + timedelta(days=i)
-        ventana_fin = min(ventana_inicio + timedelta(days=ventana - 1), datetime(fecha_inicial.year, fecha_inicial.month, dias_por_mes[fecha_inicial.strftime("%B")]))
-        fechas.append((ventana_inicio.strftime("%Y-%m-%d"), ventana_fin.strftime("%Y-%m-%d")))
-    return fechas
+    def _create_generator(self, cleaned_df, addresses_ids, config):
+        generator = GeneratorTriplet(cleaned_df, addresses_ids, config, self.logger)
+        self.logger.debug("Generator created")
+        return generator
 
-def generate_dates(src, months):
-    array_datos = []
+    def pipeline(self, config):
+        cleaned_df = self._clean_nodes()
+        addresses_ids = self._create_ids(cleaned_df)
 
-    for year in range(2023, 2024):
-        for mes in months:
-            ruta_mes = f"{src}/{year}/{mes}"
-            archivos_mes = obtener_archivos(ruta_mes)
-            array_mes = [[archivo] for archivo in archivos_mes]
-            array_datos.extend(array_mes)
-
-            archivos_mes_completo = [os.path.join(ruta_mes, f"{year}-{mes}-{day}.csv") for day in range(1, dias_por_mes[mes] + 1)]
-            array_datos.append(archivos_mes_completo)
-
-            for i in range(0, dias_por_mes[mes], 7):
-                ventana_inicio = datetime(year, months.index(mes) + 1, i + 1)
-                ventana_fin = min(ventana_inicio + timedelta(days=6), datetime(year, months.index(mes) + 1, dias_por_mes[mes]))
-
-                archivos_ventana_7_dias = []
-                for day in range(ventana_inicio.day, ventana_fin.day + 1):
-                    if day <= dias_por_mes[mes]:
-                        archivos_ventana_7_dias.append(os.path.join(ruta_mes, f"{year}-{mes}-{day}.csv"))
-
-                array_datos.append(archivos_ventana_7_dias)
-
-            for i in range(0, dias_por_mes[mes], 15):
-                ventana_inicio = datetime(year, months.index(mes) + 1, i + 1)
-                ventana_fin = min(ventana_inicio + timedelta(days=14), datetime(year, months.index(mes) + 1, dias_por_mes[mes]))
-
-                archivos_ventana_15_dias = []
-                for day in range(ventana_inicio.day, ventana_fin.day + 1):
-                    if day <= dias_por_mes[mes] and day > i:
-                        archivos_ventana_15_dias.append(os.path.join(ruta_mes, f"{year}-{mes}-{day}.csv"))
-                
-                if len(archivos_ventana_15_dias) == 1:
-                    continue
-
-                array_datos.append(archivos_ventana_15_dias)
+        self.logger.debug("Creating model")
+        model_v2_config = config["MODEL_V2"]
+        model = ModelBuilder(addresses_ids, EuclideanLoss, Adam, self.logger, model_v2_config)
+        
+        generator = self._create_generator(cleaned_df, addresses_ids, config)
+        embeddings = model.compile_model().fit(generator).get_embeddings()
+        return embeddings, addresses_ids
 
 def main(logger):
-    set_gpu(logger)
+    processing_frames = ProcessingFrames()
+    processing_frames.set_gpu(logger)
     months = ["July"]
     src = "../../../datos"
-    generated_files = generate_dates(src, months)
+    generated_files = processing_frames.generate_dates(src, months)
     for files in generated_files:
-        pipeline(files)
+        processing_frames.pipeline(files)
--- main_data_real.py
+++ main_data_real.py
@@ -2,51 +2,24 @@ import argparse
 from clustering.hdbscan.hdbscan_plot import Hdbscan
 from clustering.kmeans.kmeans_plot import Kmeans
 from logger.logger import MyLogger
-from clustering.embedders.processing_frames import pipeline_v2
+from clustering.embedders.processing_frames import ProcessingFrames
 import configparser
 import uuid
 import os
 from utils.time import Time
 from utils.file_management import FileManagement
-import numpy as np
-
-def calculate_precision(column, indexes, mapping, tmp_mapping, full_nodes, logger, file_management):
-    for i in indexes:
-        mapping[column] = mapping.get(column, {})
-        if isinstance(tmp_mapping.loc[i], np.ndarray):
-            mapping[column][tmp_mapping.loc[i][0]] = i
-        else:
-            mapping[column][tmp_mapping.loc[i]] = i
-    
-    full_nodes[f"{column}_mapped"] = full_nodes.apply(lambda x: mapping[column].get(int(x[column])), axis=1)
-
-    result = dict()
-    
-    result_act = list(full_nodes.apply(lambda x: x[column] == x.real_cluster, axis=1).value_counts())
-    if len(result_act) == 1:
-        if full_nodes.apply(lambda x: x[column] == x.real_cluster, axis=1).value_counts().index[0]:
-            logger.info(f"Precision_{column}: 1")
-            result[f"Precision_{column}"] = result.get(f"Precision_{column}", 1)
-        else:
-            logger.info(f"Precision_{column}: 0")
-            result[f"Precision_{column}"] = result.get(f"Precision_{column}", 0)
-    else:
-        precision = result.get(f"Precision_{column}", (int(result_act[1]) / (int(result_act[0])+int(result_act[1]))))
-        logger.info(f"Precision_{column}: {precision}")
-        result[f"Precision_{column}"] = precision
-
-    for_df = {}
-    for key in result.keys():
-        for_df[key] = [result[key]]
-
-    return for_df
+from clustering.embedders.processing_frames import ProcessingFrames
 
 def main(config, logger, folder_name):
     file_management = FileManagement(os.path.join(os.getcwd(), "results", folder_name))
-    
-    files = ["../datos/2023/July/2023-07-02.csv"]
 
-    embedding_matrix, ids = pipeline_v2(files, logger, config)
+    files = [
+        "../datos/2023/July/2023-07-02.csv"
+    ]
+
+    processing_frames = ProcessingFrames.build_from_files(files, logger)
+    embedding_matrix, ids = processing_frames.pipeline(config)
+
     file_management.save_npy("embedding_matrix.npy", embedding_matrix[0])
     logger.debug(f"Saved file embedding_matrix.npy")
     file_management.save_pkl("ids.pkl", ids)
